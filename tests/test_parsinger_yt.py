757f79444c65a90a451eae2622bbea84036818bb
Yakov Tereshenkov <50947571+tereshenkov29@users.noreply.github.com>
2025-04-07 23:25:31 +0300

    refactor and add second test (#51)
    * refactor add second test
    
    * Update test_parsinger_yt.py

--- tests/test_parsinger_yt.py
+++ tests/test_parsinger_yt.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 def test_result_text():
     driver = webdriver.Chrome()
     driver.get('https://parsinger.ru/selenium/3/3.2.3/index.html')
+
     button_start = driver.find_element(By.ID, "showTextBtn")
     button_start.click()
     password_text = driver.find_element(By.ID, "text1")
@@ -14,3 +15,14 @@ def test_result_text():
     result = driver.find_element(By.ID,"text2")
 
     assert result.text == "G00D-J0B-T0M-P0W3R"
+    driver.quit()
+
+def test_password_text():
+    driver = webdriver.Chrome()
+    driver.get('https://parsinger.ru/selenium/3/3.3.1/index.html')
+
+    button = driver.find_element(By.ID, "parent_id").find_element(By.CLASS_NAME, "child_class")
+    button.click()
+
+    assert button.get_attribute("password") == "GET-TH1S-C0D3"
+    driver.quit()
