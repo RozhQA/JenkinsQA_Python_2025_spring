2f9c41ee062fe40293c99e9f7248ded096fa8d61
Turii <44880961+Turii@users.noreply.github.com>
2025-04-10 17:37:26 -0700

    Test refactor (#153)

--- tests/test_login_saucedemo.py
+++ tests/test_login_saucedemo.py
@@ -2,16 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-def test_login_saucedemo():
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
 
-    driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
+class TestSauceDemo:
 
-    time.sleep(2)
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.saucedemo.com/")
 
-    assert "inventory" in driver.current_url
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        self.driver.find_element(By.ID, "login-button").click()
+        time.sleep(1)
 
-    driver.quit()
+    def teardown_method(self):
+        self.driver.quit()
+
+    def test_login_success(self):
+        assert "inventory" in self.driver.current_url
+
+    def test_add_to_cart(self):
+
+        self.driver.find_element(By.CLASS_NAME, "btn_inventory").click()
+        time.sleep(1)
+
+        cart_badge = self.driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+        assert cart_badge.text == "1"
