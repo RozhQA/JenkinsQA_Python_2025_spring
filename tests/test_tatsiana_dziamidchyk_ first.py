03cd1963eca6581d4f3300acdb8e504e3586c3c9
Tatsiana <ta.demidchik@gmail.com>
2025-04-15 23:05:18 +0300

    Correcting the test: Change site Sort by price

--- tests/test_tatsiana_dziamidchyk_ first.py
+++ tests/test_tatsiana_dziamidchyk_ first.py
@@ -3,19 +3,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+
 @pytest.fixture
-def heroku_driver():
+def driver():
     driver = webdriver.Chrome()
-    driver.get("https://the-internet.herokuapp.com/login")
+    driver.get("https://www.saucedemo.com/inventory.html")
+
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
     yield driver
     driver.quit()
 
-def test_successful_login(heroku_driver):
-    heroku_driver.find_element(By.ID, "username").send_keys("tomsmith")
-    heroku_driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
-    heroku_driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-    time.sleep(10)
+def test_sort_products_by_price(driver):
+    sort_dropdown = driver.find_element(By.CLASS_NAME, "product_sort_container")
+    sort_dropdown.click()
 
-    success_message = heroku_driver.find_element(By.ID, "flash").text
-    assert "You logged into a secure area!" in success_message
+    sort_option = driver.find_element(By.XPATH, "//option[text()='Price (low to high)']")
+    sort_option.click()
+
+    time.sleep(3)
+
+    prices = driver.find_elements(By.CLASS_NAME, "inventory_item_price")
+    price_values = []
+    for price in prices:
+        price_text = price.text
+        if "$" in price_text:
+            price_text = price_text.replace("$", "")  # Удаляем знак "$"
+        price_values.append(float(price_text))
+
+    assert price_values == sorted(price_values), "Products are not sorted by price from low to high"
+
