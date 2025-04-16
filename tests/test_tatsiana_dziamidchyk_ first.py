5c7f27b859caea606b6e0e9b30172db0b566b54b
Tatsiana <ta.demidchik@gmail.com>
2025-04-16 19:20:35 +0300

    Correcting the test, removing unnecessary fixtures

--- tests/test_tatsiana_dziamidchyk_ first.py
+++ tests/test_tatsiana_dziamidchyk_ first.py
@@ -1,25 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 import time
 
-@pytest.fixture
-def driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--incognito")  # Запуск в режиме инкогнито
-
-    driver = webdriver.Chrome(options=chrome_options)
+def test_sort_products_by_price(driver):
     driver.get("https://www.saucedemo.com/")
 
     driver.find_element(By.ID, "user-name").send_keys("standard_user")
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
     driver.find_element(By.ID, "login-button").click()
 
-    yield driver
-    driver.quit()
 
-def test_sort_products_by_price(driver):
     sort_dropdown = driver.find_element(By.CLASS_NAME, "product_sort_container")
     sort_dropdown.click()
 
