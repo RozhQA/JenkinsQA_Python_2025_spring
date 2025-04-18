d3e820a7a1e96be78bffb41f51eab328fbf87c8f
Anastasiya Pronina <apronina@piesoft.us>
2025-04-18 15:30:56 +0300

    Updates

--- tests/test_apronina_updated.py
+++ tests/test_apronina_updated.py
@@ -1,30 +1,15 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-@pytest.fixture(scope="session")
-def browser():
-    """Фикстура для управления браузером"""
-    chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(5)
-    yield driver
-    driver.quit()
-
-
 @pytest.fixture
-def sauce(browser):
+def sauce(driver):
     """Фикстура для открытия сайта"""
-    browser.get("https://www.saucedemo.com/")
-    return browser
+    driver.get("https://www.saucedemo.com/")
+    return driver
 
 
 @pytest.fixture
