60ace64f8ba719aeecfd5eb95dc21470fef72d7b
Anastasiya Pronina <apronina@piesoft.us>
2025-04-18 14:29:47 +0300

    Options changes

--- tests/test_apronina_updated.py
+++ tests/test_apronina_updated.py
@@ -11,9 +11,9 @@ from selenium.webdriver.support import expected_conditions as EC
 def browser():
     """Фикстура для управления браузером"""
     chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(5)
     yield driver
