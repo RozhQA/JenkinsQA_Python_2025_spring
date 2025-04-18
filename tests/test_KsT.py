02429e3f3afa2defae3d95885ea1d040607e5ff6
Ovchinnikov-KsT <konstemb22@gmail.com>
2025-04-18 18:29:45 +0300

    Added  test file with fixture

--- tests/test_KsT.py
+++ tests/test_KsT.py
@@ -1,26 +1,33 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from core.settings import Config
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.fixture(scope="module")
-def driver():
-    config = Config.load()
-    options = webdriver.ChromeOptions()
-    options.add_argument(config.browser.OPTIONS_CHROME)
-    driver = webdriver.Chrome(options=options)
-    yield driver
-    driver.quit()
 
-@pytest.fixture(scope="module")
-def login_success(driver):
-    driver.get("https://www.saucedemo.com/")
-    driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
-    return driver
+def test_send_contact_message(driver):
+    driver.get("https://automationintesting.online/")
+    driver.find_element(By.ID, "name").send_keys("Test User")
+    driver.find_element(By.ID, "email").send_keys("test@example.com")
+    driver.find_element(By.ID, "phone").send_keys("1234567890123")
+    driver.find_element(By.ID, "subject").send_keys("Test subject")
+    (driver.find_element(By.ID, "description").send_keys
+     ("This is a testtttttttttttttttttttttttttttttttttttttttttttttttt."))
+    button = (WebDriverWait(driver, 3).until(EC.presence_of_element_located
+                                             ((By.XPATH, "//button[text()='Submit']"))))
+    driver.execute_script("arguments[0].click();", button)
 
-def test_add_to_cart(login_success, driver):
-    driver.find_element(By.ID, 'add-to-cart-sauce-labs-bike-light').click()
-    cart = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
-    assert cart.text == "1", "not added to cart"
+    element = (driver.find_element
+    (By.XPATH, '//h3[@class="h4 mb-4" and text()="Thanks for getting in touch "]'))
+    assert element.text == "Thanks for getting in touch Test User!", "the text doesn't match"
+
+
+def test_all_room_images_are_visible(driver):
+    driver.get("https://automationintesting.online/")
+
+    images = WebDriverWait(driver, 3).until(
+    EC.presence_of_all_elements_located
+    ((By.XPATH, '//img[contains(@class, "card-img-top")]')))
+    assert images, "No room images found on this page."
+
+    for img in images:
+        assert img.is_displayed(), f"not displayed: {img.get_attribute('src')}"
+
