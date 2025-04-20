c4908cac8994657d67d1d19884a946a1d9b0d0e7
PAVEL PASHCHENKOV <142984080+ppashchenkov@users.noreply.github.com>
2025-04-20 20:46:45 +0300

    Refactoring test_create_new_project.py, (#313)
    rename directory freestyle_project

--- tests/freestyle_project/test_create_new_project.py
+++ tests/freestyle_project/test_create_new_project.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-def test_new_freestyle_project(freestyle, main_page, jenkins_reset):
+def test_new_freestyle_project(jenkins_reset, freestyle, main_page):
 
     assert main_page.find_element(By.CSS_SELECTOR, 'h1').text == "Configure"
