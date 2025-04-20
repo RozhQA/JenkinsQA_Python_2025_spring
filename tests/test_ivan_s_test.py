09d20474d259aad57623153eeeabeff8f18406fc
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-04-20 20:31:25 +0500

    failing tests galore

--- tests/test_ivan_s_test.py
+++ tests/test_ivan_s_test.py
@@ -1,7 +1,7 @@
 def test_login(login_page):
-    assert login_page.title == "Sign in [Jenkins]"
+    assert login_page.title == "Sign in [Jenkins]1"
 
 
 def test_main_page(main_page):
-    assert main_page.title == "Dashboard [Jenkins]"
+    assert main_page.title == "Dashboard1 [Jenkins]1"
 
