8b4a942771e0b89dc80247e1e0a1f36c77ffdc4d
PAVEL PASHCHENKOV <142984080+ppashchenkov@users.noreply.github.com>
2025-04-22 18:24:09 +0300

    RF_02.001.03 | Freestyle Project Configuration> Enable or Disable the Project (#347)

--- tests/freestyle_project/test_enable_disable_freestyle_project.py
+++ tests/freestyle_project/test_enable_disable_freestyle_project.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_enabled_disable_freestyle_project(freestyle):
+def test_enable_disable_switch(freestyle):
     wait = WebDriverWait(freestyle, 10)
     is_enable = freestyle.find_element(By.CLASS_NAME, 'jenkins-toggle-switch__label__checked-title')
 
@@ -15,16 +15,16 @@ def test_enabled_disable_freestyle_project(freestyle):
 
     assert is_disable.is_displayed()
 
-    freestyle.find_element(By.XPATH, '//button[@name="Submit"]').click()
-    wait.until(EC.presence_of_element_located((By.XPATH, '//button[@formnovalidate="formNoValidate"]')))
-    status_text = freestyle.find_element(By.XPATH, '//div[@class="warning"]').text.splitlines()
+def test_tooltip(tooltip):
+    title = "Enable or disable the current project"
 
-    assert status_text[0] == "This project is currently disabled"
+    assert tooltip == title
 
-    freestyle.find_element(By.XPATH, '//button[@name="Submit"]').click()
-    wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'Build Now')))
-    freestyle.find_element(By.LINK_TEXT, 'Configure').click()
-    wait.until(EC.presence_of_element_located((By.XPATH, '//label[@class="jenkins-toggle-switch__label "]')))
-    is_enable_text = freestyle.find_element(By.XPATH, '//label[@class="jenkins-toggle-switch__label "]').text
+def test_disabled_message(disabled_message):
+    warning_message = disabled_message.find_element(By.XPATH, '//div[@class="warning"]').text.splitlines()
 
-    assert is_enable_text == "Enabled"
+    assert warning_message[0] == "This project is currently disabled"
+
+def test_enable_after_disabled(enable_automatically):
+
+    assert enable_automatically == [True, True]
