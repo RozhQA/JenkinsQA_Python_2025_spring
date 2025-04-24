2e5d21fe49cdbda2fa442f0182a2bc4ceeb87e76
Iurii <yuri.ivanoff@gmail.com>
2025-04-24 13:03:40 +0300

    TC_06.001.01 | Multibranch Pipeline Configuration > Enable/Disable pipeline > Check the default state of the toggle

---
+++ tests/multibranch_pipeline_configuration/test_multibranch_pipeline_configuration.py
@@ -0,0 +1,17 @@
+import pytest
+
+from selenium.webdriver.common.by import By
+from conftest import logger
+from tests.multibranch_pipeline_configuration.multibranch_pipeline_data import Data
+
+
+@pytest.mark.new
+def test_default_state_of_the_toggle(driver, main_page):
+    driver.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
+    driver.find_element(By.ID, "name").send_keys(Data.PROJECT_NAME)
+    driver.find_element(By.CLASS_NAME, "hudson_matrix_MatrixProject").click()
+    driver.find_element(By.ID, "ok-button").click()
+
+    assert driver.find_element(
+        By.CLASS_NAME, "jenkins-toggle-switch__label__checked-title").text == "Enabled", \
+        logger.error("The wrong text of the found element!")
