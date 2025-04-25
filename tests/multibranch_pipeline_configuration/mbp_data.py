c2ec7755e70e1782f8ebe37cc66c5d2cd40d36c6
Iurii <yuri.ivanoff@gmail.com>
2025-04-25 15:02:46 +0300

    TC_06.001.02 | Multibranch Pipeline Configuration > Enable/Disable pipeline > Check Tooltip Appearance

--- tests/multibranch_pipeline_configuration/mbp_data.py
+++ tests/multibranch_pipeline_configuration/mbp_data.py
@@ -11,3 +11,5 @@ class Data:
     TOGGLE_ENABLED_TEXT = "Enabled"
     TOGGLE_DISABLED_TEXT = "Disabled"
     TOGGLE_ENABLED_ERROR_TEXT = "The toggle switch must be enabled!"
+    TOGGLE_TOOLTIP_ERROR_TEXT = "Something went wrong with the tooltip!"
+    TOGGLE_TOOLTIP_ATR = ("aria-describedby", "tooltip")
--- tests/multibranch_pipeline_configuration/test_enabled_disabled_toggle.py
+++ tests/multibranch_pipeline_configuration/test_enabled_disabled_toggle.py
@@ -1,6 +1,23 @@
+from selenium.webdriver import ActionChains
+
 from conftest import logger
 from tests.multibranch_pipeline_configuration.mbp_data import Data
 
 
 def test_default_state_of_the_toggle(driver, toggle):
     assert toggle.text == Data.TOGGLE_ENABLED_TEXT, logger.error(Data.TOGGLE_ENABLED_ERROR_TEXT)
+
+
+def test_the_tooltip_appearance(driver, toggle, toggle_tooltip, span_general):
+    action = ActionChains(driver)
+    action.move_to_element(toggle_tooltip).perform()
+    first_hovering = (toggle_tooltip.get_attribute(Data.TOGGLE_TOOLTIP_ATR[0]),
+                      toggle_tooltip.get_attribute(Data.TOGGLE_TOOLTIP_ATR[1]))
+    toggle.click()
+    action.move_to_element(span_general).perform()
+    action.move_to_element(toggle_tooltip).perform()
+    second_hovering = (toggle_tooltip.get_attribute(Data.TOGGLE_TOOLTIP_ATR[0]),
+                       toggle_tooltip.get_attribute(Data.TOGGLE_TOOLTIP_ATR[1]))
+
+    assert Data.TOGGLE_TOOLTIP_PREFS == first_hovering == second_hovering, \
+           logger.error(Data.TOGGLE_TOOLTIP_ERROR_TEXT)
