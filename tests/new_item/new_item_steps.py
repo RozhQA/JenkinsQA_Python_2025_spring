a7ff1d4a33f7aa0f8da1552589d643598b4bc655
lena <alenaxxxx94@gmail.com>
2025-04-26 14:12:01 +0300

    Adding and removing items from the cart

--- tests/new_item/new_item_steps.py
+++ tests/new_item/new_item_steps.py
@@ -8,7 +8,7 @@ def create_freestyle_item(new_item_page, config):
     wait = WebDriverWait(new_item_page, 15)
     new_item_page.find_element(*NewItem.name_field_selector).send_keys(NewItem.positive_name)
     new_item_page.find_element(*FreestyleItem.freestyle_selector).click()
-    wait.until(EC.element_to_be_clickable(FreestyleItem.ok_button_selector)).click()
+    wait.until(EC.element_to_be_clickable(NewItem.ok_button_selector)).click()
     wait.until(EC.url_to_be(config.jenkins.base_url + FreestyleItem.configuration_page))
     new_item_page.find_element(*FreestyleItem.save_button_selector).click()
     wait.until(EC.url_to_be(config.jenkins.base_url + FreestyleItem.item_page))
