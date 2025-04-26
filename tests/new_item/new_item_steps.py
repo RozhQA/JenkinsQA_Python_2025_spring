556c704414c2a79649f7100b1517b48020afd617
lena <alenaxxxx94@gmail.com>
2025-04-26 12:07:31 +0300

    Adding and removing items from the cart

--- tests/new_item/new_item_steps.py
+++ tests/new_item/new_item_steps.py
@@ -1,20 +1,17 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.remote.webdriver import WebDriver
 
 from .data_structs import NewItem, FreestyleItem
 
 
-def create_item(new_item_page, config):
+def create_freestyle_item(new_item_page, config):
+    wait = WebDriverWait(new_item_page, 15)
     new_item_page.find_element(*NewItem.name_field_selector).send_keys(NewItem.positive_name)
     new_item_page.find_element(*FreestyleItem.freestyle_selector).click()
-    wait = WebDriverWait(new_item_page, 15)
     wait.until(EC.element_to_be_clickable(FreestyleItem.ok_button_selector)).click()
-    config_url = config.jenkins.base_url + FreestyleItem.configuration_page
-    wait.until(EC.url_to_be(config_url))
+    wait.until(EC.url_to_be(config.jenkins.base_url + FreestyleItem.configuration_page))
     new_item_page.find_element(*FreestyleItem.save_button_selector).click()
-    item_url = config.jenkins.base_url + FreestyleItem.item_page
-    wait.until(EC.url_to_be(item_url))
+    wait.until(EC.url_to_be(config.jenkins.base_url + FreestyleItem.item_page))
     new_item_page.get(config.jenkins.base_url + NewItem.url)
     wait.until(EC.url_to_be(config.jenkins.base_url + NewItem.url))
 
--- tests/new_item/test_copy_from.py
+++ tests/new_item/test_copy_from.py
@@ -3,19 +3,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
 from .data_structs import FreestyleItem, NewItem
-from .new_item_steps import create_item
+from .new_item_steps import create_freestyle_item
 
 
 def test_copy_from_dropdown_shows_existing_item(new_item_page, config):
     wait = WebDriverWait(new_item_page, 10)
-    new_item_page = create_item(new_item_page, config)
-    first_letter_of_item_name = FreestyleItem.get_first_letter_of_project_name()
+    new_item_page = create_freestyle_item(new_item_page, config)
     new_item_page.find_element(*FreestyleItem.copy_from_field_selector).send_keys(
-        first_letter_of_item_name
+        FreestyleItem.get_first_letter_of_project_name()
     )
-    wait.until(EC.text_to_be_present_in_element_attribute(
-        FreestyleItem.copy_from_field_selector, "aria-expanded", "true"
-    ))
     existing_item = wait.until(EC.visibility_of_element_located((
         By.XPATH, "//a[contains(@class, 'jenkins-dropdown__item')]"
     )))
