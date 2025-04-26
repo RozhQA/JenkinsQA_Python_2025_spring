d2dffafa752b464042b7e25f0bc51f1d8f25d43f
lena <alenaxxxx94@gmail.com>
2025-04-26 14:05:36 +0300

    Adding and removing items from the cart

--- tests/new_item/data_structs.py
+++ tests/new_item/data_structs.py
@@ -8,11 +8,10 @@ class NewItem:
     name_field_selector = (By.CSS_SELECTOR, "#name")
     page_name_selector = (By.XPATH, "//h1[text()='New Item']")
     common_validation_error_selector = (By.CSS_SELECTOR, ".input-message-disabled")
-
+    ok_button_selector = (By.ID, "ok-button")
 
 class FreestyleItem:
     freestyle_selector = (By.CSS_SELECTOR, ".hudson_model_FreeStyleProject")
-    ok_button_selector = (By.ID, "ok-button")
     configuration_page = f"/job/{NewItem.positive_name}/configure"
     save_button_selector = (By.NAME, "Submit")
     item_page = f"/job/{NewItem.positive_name}/"
