8d2632a04cb8a01048add83a480c527df17c6e29
AlexandrTimo <mr.atimoshenko@gmail.com>
2025-04-26 13:44:43 -0700

    Fixed asserts in TC_02.004.01

--- tests/freestyle_project/test_freestyle_config_trigger_verif.py
+++ tests/freestyle_project/test_freestyle_config_trigger_verif.py
@@ -1,29 +1,21 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import pytest
+from tests.freestyle_project.data import TRIGGER_OPT_NAMES
 
 def test_trigger_section(freestyle):
     title_triggers = freestyle.find_element(By.ID, 'triggers')
     assert title_triggers.is_displayed
 
-def test_enable_disable_trigger_opt(freestyle):
-    my_list = []
-    bld_remote = 'pseudoRemoteTrigger'
-    my_list.append(bld_remote) 
-    bld_after_bld = 'jenkins-triggers-ReverseBuildTrigger'
-    my_list.append(bld_after_bld) 
-    bld_period = 'hudson-triggers-TimerTrigger'
-    my_list.append(bld_period) 
-    github_hook = 'com-cloudbees-jenkins-GitHubPushTrigger'
-    my_list.append(github_hook) 
-    poll_scm = 'hudson-triggers-SCMTrigger'
-    my_list.append(poll_scm) 
+@pytest.mark.parametrize("t_name", TRIGGER_OPT_NAMES)
+def test_each_trigger_opt_enable(freestyle, t_name):
+    el = freestyle.find_element(By.NAME, t_name)
+    el.send_keys(Keys.SPACE)
+    assert el.is_selected(), f"{t_name} should be enabled"
 
-    for i in my_list:
-        result = freestyle.find_element(By.NAME, i)
-        result.send_keys(Keys.SPACE)
-        assert result.is_enabled(), f"Element {i} should be enabled but it doesn't"
-
-    for i in my_list:
-        result = freestyle.find_element(By.NAME, i)
-        result.send_keys(Keys.SPACE)
-        assert result.is_selected() == False, f"Element {i} should be disbale but it doesn't"
+@pytest.mark.parametrize("t_name", TRIGGER_OPT_NAMES)
+def test_each_trigger_opt_disable(freestyle, t_name):
+    el = freestyle.find_element(By.NAME, t_name)
+    el.send_keys(Keys.SPACE)
+    el.send_keys(Keys.SPACE)
+    assert el.is_selected() == False, f"{t_name} should be disable"
