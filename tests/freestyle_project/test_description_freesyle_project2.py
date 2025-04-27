bc1f8ab71e3ec8ac11de5e8dc8f71956a9187129
pav1222 <pav333zt@gmail.com>
2025-04-27 15:06:42 +0300

    Freestyle_DescriptionTEstTC02.002.03

--- tests/freestyle_project/test_description_freesyle_project2.py
+++ tests/freestyle_project/test_description_freesyle_project2.py
@@ -2,4 +2,4 @@ from tests.freestyle_project.freestyle_data import Freestyle
 
 
 def test_text_in_description(description_appears):
-    assert Freestyle.description_text in description_appears
+    assert Freestyle.description_text == description_appears
