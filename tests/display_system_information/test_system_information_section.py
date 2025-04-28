64d5a84c801bf638d3aab7837e306f647d3d6203
Nick Tikhomirov <la8185324310@gmail.com>
2025-04-28 13:03:41 -0700

    add: test_environment_variables_tab_show_hide_each_single_value

--- tests/display_system_information/test_system_information_section.py
+++ tests/display_system_information/test_system_information_section.py
@@ -55,3 +55,18 @@ class TestSystemInformationSection:
         for element_name in element_names:
             button_locator = page.define_show_single_value_button_locator(element_name)
             assert page.is_clickable(button_locator), f'Button for {element_name} is not clickable'
+
+    def test_environment_variables_tab_show_hide_each_single_value(self, sys_info_page):
+        page = SIP(sys_info_page)
+        page.click(SI.ENVIRONMENT_VARIABLES_TAB)
+        element_names = page.get_all_element_names(SI.SHOW_ENV_VALUES_BUTTON)
+        assert len(element_names) > 0, 'List of environment variables is empty'
+
+        for element_name in element_names:
+            show_button = page.define_show_single_value_button_locator(element_name)
+            page.click(show_button)
+            assert page.is_value_displayed(element_name), f'Value for {element_name} is still hidden'
+
+            hide_button = page.define_hide_single_value_button_locator(element_name)
+            page.click(hide_button)
+            assert page.is_clickable(show_button), f'Button for {element_name} is not clickable'
