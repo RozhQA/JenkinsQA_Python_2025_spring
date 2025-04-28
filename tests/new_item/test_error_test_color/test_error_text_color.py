b3f86a99e65029100cc404437a50a16b73c7dead
yaroslavmilkevych <yaroslav08101991@gmail.com>
2025-04-28 18:04:43 +0200

    fixture added (#214)
    * fixture addeddd
    
    * Update test_herokuapp.py

--- tests/new_item/test_error_test_color/test_error_text_color.py
+++ tests/new_item/test_error_test_color/test_error_text_color.py
@@ -3,12 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-@pytest.fixture
-def wait5(main_page):
-    wait5 = WebDriverWait(main_page, 5)
-    return wait5
 
-def test_empty_name(new_item_page, wait5):
+def test_empty_name(new_item_page):
+    wait5 = WebDriverWait(new_item_page, 5)
     wait5.until(
         EC.element_to_be_clickable((By.CLASS_NAME, "jenkins-form-label"))
     ).click()
