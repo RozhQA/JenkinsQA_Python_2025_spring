70062aa0ea5cdfa347d5550fed74baf0adc77b79
Katerina Myshov <79929359+KatyaMy@users.noreply.github.com>
2025-05-02 17:09:17 -0400

    RF_15.002.02 | User > Managing Account Description > Profile Details Availibility (#547)

--- tests/user/test_user_details_configuration.py
+++ tests/user/test_user_details_configuration.py
@@ -1,19 +1,20 @@
 import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def test_account_fullname_field(main_page):
+    wait = WebDriverWait(main_page, 5)
     test_full_name = "newadmin"
 
-    main_page.find_element(By.XPATH, '//header//a[contains(@href, "user")]').click()
-    main_page.find_element(By.XPATH, '//a[contains(@href, "account")]').click()
-    full_name_field = main_page.find_element(By.XPATH, '//input[@name="_.fullName"]')
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//header//a[contains(@href, "user")]'))).click()
+    wait.until(EC.presence_of_element_located((By.XPATH, '//a[contains(@href, "account")]'))).click()
+    full_name_field = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@name="_.fullName"]')))
     full_name_field.clear()
     full_name_field.send_keys(test_full_name)
-    main_page.find_element(By.XPATH, '//button[@name="Submit"]').click()
-    account_h1_heading = main_page.find_element(By.XPATH, '//h1').text
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@name="Submit"]'))).click()
+    account_h1_heading = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.jenkins-app-bar h1"))).text
 
     assert account_h1_heading == test_full_name
 
@@ -29,6 +30,7 @@ def test_account_description_field(main_page):
     main_page.find_element(By.XPATH, '//button[@name="Submit"]').click()
 
     assert main_page.find_element(By.ID, "description").text == test_description_name
+
 
 @pytest.mark.skip
 def test_account_description_field_value(main_page):
