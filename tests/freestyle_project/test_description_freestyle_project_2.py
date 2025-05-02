71fddeee4841659634b5025d72ce3d0a509d82f2
aidar3532 <aidar3532@gmail.com>
2025-05-02 18:26:16 +0300

    Update: Making 2 asserts in 1 test

--- tests/freestyle_project/test_description_freestyle_project_2.py
+++ tests/freestyle_project/test_description_freestyle_project_2.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def test_new_freestyle(main_page):
+def test_new_freestyle_with_description(main_page):
     main_page.find_element(By.LINK_TEXT, "Create a job").click()
     wait3 = WebDriverWait(main_page, 3)
     wait3.until(EC.element_to_be_clickable((By.CLASS_NAME, "jenkins-input"))).send_keys("New Project")
@@ -19,16 +19,6 @@ def test_new_freestyle(main_page):
     wait3.until(EC.element_to_be_clickable((By.CLASS_NAME, "jenkins-submit-button"))).click()
 
     assert main_page.find_element(By.ID, "description").text == "Description"
-
-def test_new_freestyle_description_displayed(main_page):
-    main_page.find_element(By.LINK_TEXT, "Create a job").click()
-    wait3 = WebDriverWait(main_page, 3)
-    wait3.until(EC.element_to_be_clickable((By.CLASS_NAME, "jenkins-input"))).send_keys("New Project")
-    wait3.until(EC.element_to_be_clickable((By.CLASS_NAME, "hudson_model_FreeStyleProject"))).click()
-    wait3.until(EC.element_to_be_clickable((By.ID, "ok-button"))).click()
-    wait3.until(EC.presence_of_element_located((By.NAME, "description"))).send_keys("Description")
-    wait3.until(EC.element_to_be_clickable((By.CLASS_NAME, "textarea-show-preview"))).click()
-    wait3.until(EC.element_to_be_clickable((By.CLASS_NAME, "textarea-hide-preview"))).click()
-    wait3.until(EC.element_to_be_clickable((By.CLASS_NAME, "jenkins-submit-button"))).click()
-
     assert main_page.find_element(By.ID, "description").is_displayed()
+
+
