50b803a66b476121de16168f735e80c29da4ca20
LenaQA94 <lenaqa94@gmail.com>
2025-05-02 20:26:36 +0300

    RF_01.003.02 | New Item > Copy from > Display dynamic dropdown in the "Copy from"

--- tests/new_item/test_copy_from.py
+++ tests/new_item/test_copy_from.py
@@ -8,11 +8,11 @@ def test_display_existing_item(prepare_new_item, config, wait):
     prepare_new_item.find_element(*CopyFrom.copy_from_selector).send_keys(
         CopyFrom.first_letter_of_name()
     )
-    element = wait.until(EC.visibility_of_element_located((
+    item_element = wait.until(EC.visibility_of_element_located((
         By.CSS_SELECTOR, "a.jenkins-dropdown__item"))
     )
-    existing_item = element.text.strip()
+    actual_text = item_element.text.strip()
 
-    assert existing_item == NewItem.positive_name, (
-        f"Expected item '{NewItem.positive_name}', but found '{existing_item}'"
+    assert actual_text == NewItem.positive_name, (
+        f"Expected item '{NewItem.positive_name}', but found '{actual_text}'"
     )
