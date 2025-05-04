280f3166831babbde1717e27b95e808c52d2b8e5
Nick Tikhomirov <la8185324310@gmail.com>
2025-05-04 01:08:20 -0700

    feat: add test_open_system_information_page

---
+++ tests/manage_jenkins/status_information/test_system_information.py
@@ -0,0 +1,8 @@
+from pages.manage_jenkins.status_information.system_information_page import SystemInformationPage as SI
+
+
+def test_open_system_information_page(manage_jenkins_page):
+    system_information_page = manage_jenkins_page.go_to_the_system_information_page()
+    assert system_information_page.is_visible(SI.Locator.TABS_BAR)
+
+
