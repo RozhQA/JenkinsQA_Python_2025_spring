c43d8a6957fe15c29df7b432b036c9e735943200
Nick Tikhomirov <la8185324310@gmail.com>
2025-05-04 08:45:35 -0700

    rfr: del mark.xfail

--- tests/new_item/test_new_folder.py
+++ tests/new_item/test_new_folder.py
@@ -2,7 +2,7 @@ import pytest
 from pages.new_item_page import NewItemPage
 from tests.new_item.data import new_folder_name
 
-@pytest.mark.xfail
+
 def test_create_new_folder(new_item_page: NewItemPage):
     items = new_item_page.create_new_folder(new_folder_name).go_to_the_main_page().get_item_list()
     assert items == [new_folder_name], f"Folder '{new_folder_name}' NOT FOUND"
