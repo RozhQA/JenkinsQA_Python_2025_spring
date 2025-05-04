5bcba90a271eb239dee5d91422d068dbb630a269
Nick Tikhomirov <la8185324310@gmail.com>
2025-05-04 01:07:29 -0700

    feat: add SystemInformationPage

---
+++ pages/manage_jenkins/status_information/system_information_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from pages.manage_jenkins.manage_jenkins_page import ManageJenkinsPage as MJ
+
+
+class SystemInformationPage(MJ):
+    class Locator:
+        TABS_BAR = (By.CSS_SELECTOR, ".tabBar")
+
+    def __init__(self, driver, timeout=5):
+        super().__init__(driver, timeout=timeout)
+        self.url += "systemInfo"
