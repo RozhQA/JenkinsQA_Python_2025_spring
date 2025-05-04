f83945711d21ab177f66d398555749126430e9ee
Nick Tikhomirov <la8185324310@gmail.com>
2025-05-04 09:02:23 -0700

    rfr: del display_system_information folder

--- tests_to_refactor/display_system_information/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from tests.display_system_information.system_information_page import SystemInformationPage as SI
-
-
-@pytest.fixture(scope='function')
-def sys_info_page(main_page):
-    main_page.get(main_page.current_url + SI.ENDPOINT_URL)
-    return main_page
