6321f17fb406c30e7a77918de6391d0fb85dfaac
Nick Tikhomirov <la8185324310@gmail.com>
2025-05-04 01:17:28 -0700

    rfr: add assert message

--- tests/manage_jenkins/status_information/test_system_information.py
+++ tests/manage_jenkins/status_information/test_system_information.py
@@ -3,6 +3,6 @@ from pages.manage_jenkins.status_information.system_information_page import Syst
 
 def test_open_system_information_page(manage_jenkins_page):
     system_information_page = manage_jenkins_page.go_to_the_system_information_page()
-    assert system_information_page.is_visible(SI.Locator.TABS_BAR)
+    assert system_information_page.is_visible(SI.Locator.TABS_BAR), "Tabs bar is not displayed"
 
 
