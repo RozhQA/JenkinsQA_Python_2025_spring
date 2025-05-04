17a0ed377f0d98cf54dfacea19e3f4ef3d278585
Igor <raynerus@gmail.com>
2025-05-05 02:31:38 +0900

    ref test

--- tests/new_item/test_project_type_selected.py
+++ tests/new_item/test_project_type_selected.py
@@ -1,14 +1,20 @@
 from pages.new_item_page import NewItemPage
-from tests.new_item.data import project_type_freestyle_title
 
 
 def test_only_one_project_can_be_selected(new_item_page: NewItemPage):
-    new_item_page.select_pipeline_project()
-    new_item_page.select_freestyle_project()
-    selected_items = new_item_page.get_selected_items()
-    highlighted_items = new_item_page.get_highlighted_items()
-    highlighted_title = new_item_page.get_title_highlighted_item()
+    page = new_item_page
+    pipeline_item = page.get_pipeline_element()
+    page.click_element(pipeline_item)
+
+    assert page.get_active_element() == pipeline_item
+
+    freestyle_item = page.get_freestyle_element()
+    page.click_element(freestyle_item)
+
+    assert page.get_active_element() == freestyle_item
+
+    selected_items = page.get_selected_items()
+    highlighted_items = page.get_highlighted_items()
 
     assert len(selected_items) == 1
     assert highlighted_items == selected_items
-    assert highlighted_title == project_type_freestyle_title
