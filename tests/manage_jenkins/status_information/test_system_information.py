3604308eb663ae89814c526be90532d633bcf3c6
Nick Tikhomirov <la8185324310@gmail.com>
2025-05-05 15:32:16 -0700

    rfr: test_open_system_information_page due to move test data to data.py

--- tests/manage_jenkins/status_information/test_system_information.py
+++ tests/manage_jenkins/status_information/test_system_information.py
@@ -1,6 +1,6 @@
-from pages.manage_jenkins.status_information.system_information_page import SystemInformationPage as SI
+from pages.manage_jenkins.status_information.data import SystemInformationData as SI
 
 
 def test_open_system_information_page(manage_jenkins_page):
     sys_info_page = manage_jenkins_page.go_to_sys_info_page()
-    assert sys_info_page.get_tabs_bar_headers() == SI.Data.TABS_BAR_HEADERS
+    assert sys_info_page.get_tabs_bar_headers() == SI.TABS_BAR_HEADERS
