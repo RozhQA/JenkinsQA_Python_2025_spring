b7b022f2a4a82bdd652a1cc9accd73604908b8b8
Nick Tikhomirov <la8185324310@gmail.com>
2025-05-05 23:15:42 -0700

    rfr: rename method to go_to_system_information_page

--- tests/manage_jenkins/status_information/test_system_information.py
+++ tests/manage_jenkins/status_information/test_system_information.py
@@ -2,5 +2,5 @@ from pages.manage_jenkins.status_information.data import SystemInformationData a
 
 
 def test_open_system_information_page(manage_jenkins_page):
-    sys_info_page = manage_jenkins_page.go_to_sys_info_page()
+    sys_info_page = manage_jenkins_page.go_to_system_information_page()
     assert sys_info_page.get_tabs_bar_headers() == SI.TABS_BAR_HEADERS
