e00015078eb161dd442b4cd84801165bd3f7af8b
Igor <raynerus@gmail.com>
2025-05-07 23:47:36 +0900

    add locator

--- pages/new_item_page.py
+++ pages/new_item_page.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
+
 from pages.base_page import BasePage
+
 
 class NewItemPage(BasePage):
     class Locator:
@@ -13,6 +15,7 @@ class NewItemPage(BasePage):
         ERROR_MESSAGE = (By.ID, "itemname-required")
         ITEM_MULTI_CONFIG_PROJECT = (By.CLASS_NAME, "hudson_matrix_MatrixProject")
         ITEM_TYPES = (By.CSS_SELECTOR, ".label")
+        ITEM_DESCRIPTIONS = (By.XPATH, "//div[@class='desc']")
 
     def __init__(self, driver, timeout=5):
         super().__init__(driver, timeout=timeout)
@@ -63,6 +66,9 @@ class NewItemPage(BasePage):
         self.wait_to_be_clickable(self.Locator.OK_BUTTON).click()
         return MultiConfigProjectConfigPage(self.driver, name).wait_for_url()
 
-    def get_item_types_text(self):
+    def get_item_type_names(self):
         elements = self.wait_to_be_visible_all(self.Locator.ITEM_TYPES)
         return [element.text for element in elements]
+
+    def get_item_type_descriptions(self):
+        return [desc.text.strip() for desc in self.find_elements(*self.Locator.ITEM_DESCRIPTIONS)]
