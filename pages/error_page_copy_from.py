e65a5ec6668242efdd69262c1bd7767c56fd947a
LenaQA94 <lenaqa94@gmail.com>
2025-05-09 17:58:58 +0300

    tc_01.003.04

--- pages/error_page_copy_from.py
+++ pages/error_page_copy_from.py
@@ -5,15 +5,15 @@ from pages.base_page import BasePage
 
 class ErrorPageCopyFrom(BasePage):
     class Locators:
-        ERROR_HEADER = (By.CSS_SELECTOR, "#main-panel > h1")
-        ERROR_TEXT = (By.CSS_SELECTOR, "#main-panel > p")
+        HEADER_ERROR= (By.CSS_SELECTOR, "#main-panel > h1")
+        MESSAGE_ERROR = (By.CSS_SELECTOR, "#main-panel > p")
 
     def __init__(self, driver, timeout=5):
         super().__init__(driver, timeout)
         self.url = self.base_url + "/view/all/createItem"
 
     def get_header_error(self):
-        return self.wait_for_element(self.Locators.ERROR_HEADER).text.strip()
+        return self.wait_for_element(self.Locators.HEADER_ERROR).text.strip()
 
     def get_message_error(self):
-        return self.wait_for_element(self.Locators.ERROR_TEXT).text.strip()
+        return self.wait_for_element(self.Locators.MESSAGE_ERROR).text.strip()
