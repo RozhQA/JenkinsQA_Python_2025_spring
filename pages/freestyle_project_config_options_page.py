a210b49231c0721e350dd36bbaa6dc50eba19bea
AlexandrTimo <mr.atimoshenko@gmail.com>
2025-05-09 20:57:53 -0700

    Added RF_02.004.01

---
+++ pages/freestyle_project_config_options_page.py
@@ -0,0 +1,31 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class FreestylePJConfOptPage(BasePage):
+    def __init__(self, freestyle):
+        super().__init__(freestyle)
+
+    TRIGGER_OPT_NAMES = [
+        'pseudoRemoteTrigger',
+        'jenkins-triggers-ReverseBuildTrigger',
+        'hudson-triggers-TimerTrigger',
+        'com-cloudbees-jenkins-GitHubPushTrigger',
+        'hudson-triggers-SCMTrigger'
+    ]
+
+    def check_trigger_title(self):
+        title_triggers = self.find_element(By.ID, 'triggers')
+        return title_triggers.is_displayed
+
+    def check_box_enable(self, option_name: str):
+        el = self.find_element(By.NAME, option_name)
+        el.send_keys(Keys.SPACE)
+        return el.is_selected()
+    
+    def check_box_disable(self, option_name: str):
+        el = self.find_element(By.NAME, option_name)
+        el.send_keys(Keys.SPACE)
+        el.send_keys(Keys.SPACE)
+        return el.is_selected()
---
+++ tests/freestyle_project/test_freestyle_config_trigger_verif.py
@@ -0,0 +1,18 @@
+import pytest
+from pages.freestyle_project_config_options_page import FreestylePJConfOptPage
+
+def test_trigger_section(freestyle):
+    freestyle_pj_conf_page = FreestylePJConfOptPage(freestyle)
+    assert freestyle_pj_conf_page.check_trigger_title
+
+@pytest.mark.parametrize("t_name", FreestylePJConfOptPage.TRIGGER_OPT_NAMES)
+def test_each_trigger_opt_enable(freestyle, t_name):
+    freestyle_pj_conf_page = FreestylePJConfOptPage(freestyle)
+    el = freestyle_pj_conf_page.check_box_enable(t_name)
+    assert el, f"{t_name} should be enabled"
+
+@pytest.mark.parametrize("t_name", FreestylePJConfOptPage.TRIGGER_OPT_NAMES)
+def test_each_trigger_opt_disable(freestyle, t_name):
+    freestyle_pj_conf_page = FreestylePJConfOptPage(freestyle)
+    el = freestyle_pj_conf_page.check_box_disable(t_name)
+    assert el == False, f"{t_name} should be disable"
