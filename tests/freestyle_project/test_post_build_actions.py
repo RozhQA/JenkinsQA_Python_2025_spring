87c227d3d1ec68e70357bfc2367d8f774c747360
AlexandrTimo <mr.atimoshenko@gmail.com>
2025-05-12 11:42:58 -0700

    removed ruff issue

--- tests/freestyle_project/test_post_build_actions.py
+++ tests/freestyle_project/test_post_build_actions.py
@@ -5,7 +5,7 @@ def test_post_build_actions_is_available(freestyle):
     freestyle.scroll_to_bottom_screen()
     assert freestyle.get_port_build_actions_element().is_displayed()
 
-
+    
 def test_add_post_build_actions(freestyle):
     freestyle.scroll_to_bottom_screen()
     freestyle.click_add_post_build_actions()
