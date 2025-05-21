3e5ba642021aa8219cbe3658d04adb05ebc2cbc9
Space-researcher <space-researcher@i.ua>
2025-05-21 22:14:21 +0300

    waiter update

--- pages/folder_config_page.py
+++ pages/folder_config_page.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from pages.base_page import BasePage
 from pages.folder_page import FolderPage
 
