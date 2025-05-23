d70286fcd4b13dbbcbe01bb6ac6ad50f82c57d7f
Igor <raynerus@gmail.com>
2025-05-23 15:58:29 +0900

    TC_12.001_02 | Dashboard with the items > View All Jobs > Verification of Column Headers in Dashboard Table

--- tests/dashboard/test_view_all_jobs.py
+++ tests/dashboard/test_view_all_jobs.py
@@ -1,10 +1,10 @@
 import allure
 
-
+from tests.dashboard.data import DashboardTable
 from tests.new_item.data import new_folder_name, new_pipeline_name, new_multi_config_project_name
 
 
-@allure.epic("Dashboard")
+@allure.epic("Dashboard with the items")
 @allure.story("View All Jobs")
 @allure.title("Display list of jobs and folders")
 @allure.description("Verify that all jobs and folders are listed on the Dashboard page.")
@@ -18,3 +18,16 @@ def test_all_jobs_and_folders_visible_in_dashboard(create_job_and_folders_env, m
 
     folder_contents = main_page.go_to_the_folder_page(new_folder_name).get_item_list()
     assert new_pipeline_name in folder_contents, "Pipeline job should be visible when opening its folder"
+
+
+@allure.epic("Dashboard with the items")
+@allure.story("View All Jobs")
+@allure.title("Verification of column headers in dashboard table")
+@allure.description("Verify the dashboard table displays correct abbreviated column headers.")
+@allure.testcase("TC_12.001.02")
+@allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/742", name="Github issue")
+def test_verify_table_column_headers(create_job_pipeline_env, main_page):
+    actual_headers = main_page.get_table_headers_list()
+    expected_headers = list(DashboardTable.HEADERS_MAP.values())
+    assert actual_headers, "Headers list is empty or not returned"
+    assert actual_headers == expected_headers, f"Headers mismatch. Expected:{expected_headers}, actual:{actual_headers}"
