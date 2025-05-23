8ed158f87a3c247585db48145251ec53c3b55840
LenaQA94 <lenaqa94@gmail.com>
2025-05-24 00:19:18 +0300

    tc_04.001.05

--- pages/multi_config_project_page.py
+++ pages/multi_config_project_page.py
@@ -1,5 +1,7 @@
-from pages.base_page import BasePage
+import allure
 from selenium.webdriver.common.by import By
+
+from pages.base_page import BasePage
 
 
 class MultiConfigProjectPage(BasePage):
@@ -11,10 +13,12 @@ class MultiConfigProjectPage(BasePage):
         WARNING_MESSAGE = (By.ID, "enable-project")
         ENABLE_BUTTON = (By.XPATH, "//form[@id='enable-project']//button")
         PROJECT_STATUS_ICON = (By.CSS_SELECTOR, "#matrix svg.icon-md")
+        CONFIGURE_MENU_ITEM = (By.XPATH, "//span[text()='Configure']/..")
 
     def __init__(self, driver, name, timeout=5):
         super().__init__(driver, timeout=timeout)
         self.url = self.base_url + f"/job/{name}/"
+        self.name = name
 
     def get_saved_description_text(self):
         return self.wait_to_be_visible(self.Locators.SAVED_DESCRIPTION).text
@@ -39,3 +43,9 @@ class MultiConfigProjectPage(BasePage):
 
     def get_project_status_title(self) -> str:
         return self.wait_and_get_attribute(self.Locators.PROJECT_STATUS_ICON, "title")
+
+    def go_to_configure_page(self):
+        with allure.step(f'Go to Configure page of project "{self.name}"'):
+            from pages.multi_config_project_config_page import MultiConfigProjectConfigPage
+            self.click_on(self.Locators.CONFIGURE_MENU_ITEM)
+            return MultiConfigProjectConfigPage(self.driver, self.name).wait_for_url()
