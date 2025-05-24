0352561ba7a15ef2f3879e8794bedc4ef1e6b2cd
LenaQA94 <lenaqa94@gmail.com>
2025-05-24 19:16:05 +0300

    rf_01.003.04

--- tests/multi_config/data.py
+++ tests/multi_config/data.py
@@ -3,8 +3,11 @@ project_name = "MyProject"
 updated_description_text = "Updated project summary"
 
 
-class ProjectToggle:
+class Project:
     PROJECT_NAME = "Multi-configuration project"
+
+
+class ProjectToggle:
     WARNING_MESSAGE = "This project is currently disabled"
     STATUS_NOT_BUILT = "Not built"
     TOOLTIP = "Enable or disable the current project"
