68066b147c8e3eed5a964bffe6a04330b538bf9a
LenaQA94 <lenaqa94@gmail.com>
2025-05-29 20:06:24 +0300

    delete allure step

--- tests/pipeline/conftest.py
+++ tests/pipeline/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-import allure
 
 from tests.pipeline.pipeline_data import pipeline_project_name, BuildTriggers
 from pages.pipeline_config_page import PipelineConfigPage
@@ -13,6 +12,5 @@ def pipeline_config_page(main_page):
 
 
 @pytest.fixture(scope="function")
-@allure.title("Create Pipline project")
 def pipline_project_config_page(new_item_page) -> PipelineConfigPage:
     return new_item_page.create_new_pipeline_project(BuildTriggers.PROJECT_NAME)
