6d3b1103af7b12b742f34fad1c4462d42dae2ad7
Igor <raynerus@gmail.com>
2025-05-29 14:51:46 +0900

    RF_01.002.02 | New Item > Select an Item type > Display of description each type

--- tests/new_item/test_item_type_check_descriptions.py
+++
@@ -1,8 +0,0 @@
-from pages.new_item_page import NewItemPage
-from tests.new_item.data import expected_item_descriptions
-
-
-def test_display_description_of_item_type(new_item_page: NewItemPage):
-    assert new_item_page.get_item_type_descriptions() == expected_item_descriptions, "Incorrect item descriptions"
-    assert len(new_item_page.get_item_type_descriptions()) == len(new_item_page.get_item_type_names()), \
-        "Descriptions count mismatch"
