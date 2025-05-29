863da002c4970246f7e6886b52834238cfad93b9
Igor <raynerus@gmail.com>
2025-05-29 15:23:24 +0900

    fix link

--- tests/new_item/test_select_an_item_type.py
+++ tests/new_item/test_select_an_item_type.py
@@ -10,7 +10,7 @@ from tests.new_item.data import expected_item_descriptions
 @allure.title("Display of description each type")
 @allure.description("Verify that the 'New Item' screen displays a description of each type of project.")
 @allure.testcase("TC_01.002.02")
-@allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/79", name="Github issue")
+@allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/332", name="Github issue")
 def test_display_description_of_item_type(new_item_page: NewItemPage):
     actual_names = new_item_page.get_item_type_names()
     actual_descriptions = new_item_page.get_item_type_descriptions()
