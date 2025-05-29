e6b3c71b87236eebf502b491d222b3740a031395
LenaQA94 <lenaqa94@gmail.com>
2025-05-29 20:17:34 +0300

    tc_03.003.02

--- tests/pipeline/pipeline_data.py
+++ tests/pipeline/pipeline_data.py
@@ -5,3 +5,5 @@ pipeline_project_name = "First Pipeline Project"
 class BuildTriggers:
     PROJECT_NAME = "Pipeline project"
     TITLE = "Triggers"
+    DESCRIPTION = ("Set up automated actions that start your build based on specific events, "
+                   "like code changes or scheduled times")
