be5092f7f7c57145c3413dbd4b822144e53fb376
LenaQA94 <lenaqa94@gmail.com>
2025-05-30 15:34:20 +0300

    rename fixture

--- tests/pipeline/test_build_triggers.py
+++ tests/pipeline/test_build_triggers.py
@@ -8,8 +8,8 @@ from tests.pipeline.pipeline_data import BuildTriggers
 @allure.title("Displaying the \"Triggers\" section title")
 @allure.testcase("TC_03.003.01")
 @allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/822", name="Github issue")
-def test_display_triggers_section_title(pipline_project_config_page):
-    assert pipline_project_config_page.get_title_triggers() == BuildTriggers.TITLE, \
+def test_display_triggers_section_title(pipeline_project_config_page):
+    assert pipeline_project_config_page.get_title_triggers() == BuildTriggers.TITLE, \
         f"Expected title '{BuildTriggers.TITLE}' NOT FOUND"
 
 
@@ -18,8 +18,8 @@ def test_display_triggers_section_title(pipline_project_config_page):
 @allure.title("Displaying the \"Triggers\" section description")
 @allure.testcase("TC_03.003.02")
 @allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/831", name="Github issue")
-def test_display_triggers_section_description(pipline_project_config_page):
-    assert pipline_project_config_page.get_description_triggers() == BuildTriggers.DESCRIPTION, \
+def test_display_triggers_section_description(pipeline_project_config_page):
+    assert pipeline_project_config_page.get_description_triggers() == BuildTriggers.DESCRIPTION, \
         f"Expected description '{BuildTriggers.DESCRIPTION}' NOT FOUND"
 
 
@@ -28,6 +28,6 @@ def test_display_triggers_section_description(pipline_project_config_page):
 @allure.title("Displaying the \"Triggers\" section in the sidebar")
 @allure.testcase("TC_03.003.03")
 @allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/832", name="Github issue")
-def test_display_triggers_section_in_sidebar(pipline_project_config_page):
-    assert pipline_project_config_page.get_sidebar_triggers() == BuildTriggers.TITLE, \
+def test_display_triggers_section_in_sidebar(pipeline_project_config_page):
+    assert pipeline_project_config_page.get_sidebar_triggers() == BuildTriggers.TITLE, \
         f"Expected title in the sidebar '{BuildTriggers.TITLE}' NOT FOUND"
