bdaff3b45f44f3e828d4404cb69d0de40abcaac2
LenaQA94 <lenaqa94@gmail.com>
2025-06-11 15:03:39 +0300

    tc_03.003.14

--- pages/pipeline_config_page.py
+++ pages/pipeline_config_page.py
@@ -20,7 +20,7 @@ class PipelineConfigPage(BasePage):
         TRIGGER_BUILD_AFTER_OTHER_PROJECTS_LABEL = (By.XPATH, "//input[@id='cb8']/following-sibling::label")
         TRIGGER_PROJECTS_INPUT = (By.NAME, "_.upstreamProjects")
         TRIGGER_PROJECTS_INPUT_LABEL = (By.CSS_SELECTOR, "div[nameref='cb8'] .help-sibling")
-        TRIGGER_PROJECTS_INPUT_ERROR = (By.CSS_SELECTOR, 'div[nameref="cb8"] .error')
+        TRIGGER_PROJECTS_INPUT_ERROR = (By.CSS_SELECTOR, "div[nameref='cb8'] .error")
 
     def __init__(self, driver, pipeline_name, timeout=5):
         super().__init__(driver, timeout=timeout)
