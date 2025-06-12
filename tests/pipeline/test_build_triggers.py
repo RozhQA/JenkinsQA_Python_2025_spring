021b541b149f32c768460cb8cf6a305512e180b0
LenaQA94 <lenaqa94@gmail.com>
2025-06-12 12:29:36 +0300

    tc_03.003.15

--- tests/pipeline/test_build_triggers.py
+++ tests/pipeline/test_build_triggers.py
@@ -171,7 +171,7 @@ def test_projects_to_watch_default_empty_and_error(pipeline_config_page_enable_b
 @allure.title("Labels of trigger radio buttons are correct")
 @allure.testcase("TC_03.003.15")
 @allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/862", name="Github issue")
-def test_display_radio_buttons_and_labels(pipeline_config_page_enable_build_after_checkbox):
+def test_display_radio_button_labels(pipeline_config_page_enable_build_after_checkbox):
     actual_labels = pipeline_config_page_enable_build_after_checkbox.get_radio_button_labels()
 
     assertions.soft_assert_list_length_equal(actual_labels, BuildTriggers.RADIO_BUTTON_LABELS)
