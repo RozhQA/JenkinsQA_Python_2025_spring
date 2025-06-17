8e1f9dee4aa83167126bc655bb7222b56ac2c97e
Igor <raynerus@gmail.com>
2025-06-17 18:32:24 +0900

    ref title allure

--- tests/api/tests_ui/dashboard/test_view_all_jobs.py
+++ tests/api/tests_ui/dashboard/test_view_all_jobs.py
@@ -5,12 +5,12 @@ from tests.api.tests_ui.dashboard.data import DashboardTable
 
 @allure.epic("Dashboard with the items")
 @allure.story("View All Jobs")
-@allure.title("Verification of default state for job without configuration")
+@allure.title("API:Verification of default state for job without configuration")
 @allure.description("Verify that a newly created job without any build history or configuration displays the correct"
                     " default state in the dashboard jobs table.")
 @allure.testcase("TC_12.001.03")
 @allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/757", name="Github issue")
-def test_verify_default_state_for_all_projects1(main_page, create_projects_via_api):
+def test_verify_default_state_for_all_project(main_page, create_projects_via_api):
     allure.attach(
         main_page.driver.get_screenshot_as_png(),
         name="Dashboard view with all created projects",
